d52ff483c9e2e6bde3cd7060202d270ff4103705
nicomateucci <nico-mateucci@hotmail.com>
2019-11-27 11:03:03 -0300

    Agregue forms de Angular

--- frontend/src/app/componentes/nueva-tarea/nueva-tarea.component.ts
+++ frontend/src/app/componentes/nueva-tarea/nueva-tarea.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, Validators} from '@angular/forms';
+import { TareaService} from '../../servicios/tarea.service';
+import { Tarea } from '../../modelos/tarea';
 
 @Component({
   selector: 'app-nueva-tarea',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nueva-tarea.component.css']
 })
 export class NuevaTareaComponent implements OnInit {
+  constructor(
+    private tareaService: TareaService,
+  ) {}
 
-  constructor() { }
+  // formControl = new FormControl('formTarea');
+  tareaForm = new FormGroup({
+    // titulo: new FormControl('', [this.createCustomValidator()]),
+    titulo: new FormControl('', [Validators.required]),
+    descripcion: new FormControl('', Validators.maxLength(50)),
+    fecha: new FormControl('', [Validators.required]),
+  }, [Validators.required]);
 
   ngOnInit() {
+  }
+
+  onSubmit(data) {
+    // Process checkout data here
+    console.warn('Your order has been submitted', data);
+    console.log(this.tareaForm.value);
+    // tarea = new Tarea();
+    // tarea.titulo = data
+
+    this.tareaForm.reset();
   }
 
 }
